fe6e6e5ec6381fb205e8e2829bfe1d71b6b2a564
xingxing <1710563799@qq.com>
2022-04-02 20:55:29 +0800

    first commit

--- src/main/java/com/atlixing/gitDemo.java
+++ src/main/java/com/atlixing/gitDemo.java
@@ -4,5 +4,6 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello Git");
+        System.out.println("hello hit-fix");
     }
 }
